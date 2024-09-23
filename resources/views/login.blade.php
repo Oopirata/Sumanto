fd5db1d3d0240bacb0527fa3905466e1ee46d743
Oopirata <herovva@gmail.com>
2024-09-23 18:21:35 +0700

    Refactor main.blade.php and login.blade.php views

---
+++ resources/views/login.blade.php
@@ -0,0 +1,20 @@
+@extends('main')
+
+@section('title', 'Login')
+
+@section('page')
+<div class="login-container">
+    <img src="logo.png" alt="Logo">
+    <h3>Sistem Unggulan Manajemen Akademik dan Terintegrasi Online</h3>
+    <h2>Log in menggunakan SSO</h2>
+    <form action="login.php" method="post">
+        <input type="email" name="email" placeholder="Masukkan E-Mail" required>
+        <input type="password" name="password" placeholder="Masukkan Password" required>
+        <button type="submit">Masuk</button>
+    </form>
+    <a href="#">
+        <span class="lupa-password">Lupa password?</span>
+        <span class="reset-password"> RESET PASSWORD</span>
+    </a>
+</div>
+@endsection
