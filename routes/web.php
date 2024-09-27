fbc0c5a4145188d63d8d2e95fdcd727c63caf40e
Muhammad Mirza Faiz Rabbani <mirzafaizrabbani@students.undip.ac.id>
2024-09-27 17:05:05 +0700

    asu

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,7 @@ Route::get('/', function () {
     return view('login');
 });
 
-<<<<<<< HEAD
+
 Route::get('/tes', function () {
     return view('tes');
-=======
-Route::get('/sidebar', function () {
-    return view('sidebar');
->>>>>>> e7fbc998e8b8fe49d119ab7e40c43915d308e92f
 });
