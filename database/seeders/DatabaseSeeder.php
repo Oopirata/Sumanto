998073087c4da0007c1945d1d74048b862a6c9cb
Oopirata <herovva@gmail.com>
2024-10-26 21:51:04 +0700

    Refactor DatabaseSeeder to include additional seeders

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $this->call([
+            JadwalSeeder::class,
+            KhsSeeder::class,
+            RoleSeeder::class,
+            UserSeeder::class,
+            RoleUserSeeder::class,
+            RuanganSeeder::class,
+        ]);
+    }
+}
