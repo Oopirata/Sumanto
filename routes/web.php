a22cc828c2334d13a5f1f2400554b9aa90d08bc0
Muhammad Mirza Faiz Rabbani <mirzafaizrabbani@students.undip.ac.id>
2024-11-05 20:37:56 +0700

    masuk

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\DosenMatakuliahController;
-use App\Http\Controllers\BuatIRSController;
-use App\Http\Controllers\IRSController;
 use App\Http\Controllers\JadwalController;
+use App\Http\Controllers\MatakuliahController;
 use Illuminate\Support\Facades\Route;
 
 // Route for displaying the login page
@@ -15,6 +14,8 @@ Route::get('/', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/', function () {
     return view('login');
 })->name('login');
+
+Route::get('/buat-irs', [JadwalController::class, 'createIRS'])->name('buat.irs');
 
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 
@@ -33,6 +34,9 @@ Route::middleware(['auth'])->group(function () {
 // Dashboard Routes with middleware
 // Route::middleware(['auth'])->group(function () {
 // Student dashboard
+Route::get('mhs/dashboard', function () {
+    return view('mhsDashboard');
+})->name('mhs.dashboard');
 
 // Dean dashboard
 Route::get('dekan/dashboard', function () {
@@ -76,11 +80,14 @@ Route::get('kaprodij', function () {
     return view('kaprodiBuatJadwal');
 });
 
-Route::get('mhs/dashboard', [MhsDashboard::class, 'dashboardMhs'])->name('mhs.dashboard');
+Route::get('/mhs/BuatIrs', function () {
+    return view('mhsBuatIrs');
+});
 
-Route::get('/mhs/BuatIrs', [BuatIRSController::class, 'tampil_jadwal'])->name('buat.irs');
 
-Route::get('/mhs/irs', [IRSController::class, 'all'])->name('mhs.irs');
+Route::get('/mhs/irs', function () {
+    return view('mhsIrs');
+});
 
 Route::get('/mhs/khs', function () {
     return view('mhsKhs');
