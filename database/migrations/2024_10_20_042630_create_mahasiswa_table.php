42edf4d04117ab8bd544e350131699a121413328
Bintang Syafrian Rizal <bintang.syafrian@gmail.com>
2024-11-18 09:11:05 +0700

    nothing :wq

--- database/migrations/2024_10_20_042630_create_mahasiswa_table.php
+++ database/migrations/2024_10_20_042630_create_mahasiswa_table.php
@@ -32,7 +32,10 @@ return new class extends Migration
             $table->string('semester');
             $table->string('prodi');
             $table->float('IPK', 2)->nullable();
-            $table->foreignId('dosen_wali_id')->constrained('dosen')->onDelete('cascade')->nullable();
+            $table->float('IPS', 2)->nullable();
+            $table->unsignedBigInteger('dosen_wali_id');
+
+            $table->foreign('dosen_wali_id')->references('id')->on('dosen')->onDelete('cascade');
             $table->timestamps();
         });
     }
